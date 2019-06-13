ba8ebf5c8af7e0106432d5eac085641e6834f065
NicolasOC <nicolas_fe_ortiz@hotmail.com>
2019-06-13 00:02:52 -0400

    last update corriendo bien, falta mejorar interfaz

--- app/src/main/java/com/example/gddlogin/MainActivity.java
+++ app/src/main/java/com/example/gddlogin/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.gddlogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button go_to_contact = findViewById(R.id.go_to_contact_id);
+        go_to_contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), Contacto.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
